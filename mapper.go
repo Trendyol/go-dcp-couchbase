55cacb84e272e18dd11bfd7d2c51dbfa9179450e
Eray <relfishere@gmail.com>
2023-07-12 04:00:54 +0300

    fix: DefaultMapper

--- mapper.go
+++ mapper.go
@@ -6,7 +6,7 @@ type Mapper func(event couchbase.Event) []couchbase.CBActionDocument
 
 func DefaultMapper(event couchbase.Event) []couchbase.CBActionDocument {
 	if event.IsMutated {
-		return []couchbase.CBActionDocument{couchbase.NewMutateInAction(event.Key, event.Key, event.Value)}
+		return []couchbase.CBActionDocument{couchbase.NewSetAction(event.Key, event.Value)}
 	}
 	return []couchbase.CBActionDocument{couchbase.NewDeleteAction(event.Key)}
 }
